e7e68ae3bf4bb718f1cf9186b7e12e6e2d0374ec
Grzegorz Baran <bn.grzegorz@gmail.com>
2017-03-08 18:14:27 +0100

    bledy js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -122,22 +122,6 @@ $(window).load(function() {
 });
 
 
-var lineChartData = {
-		labels : ["January","February","March","April","May","June","July"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [65,59,90,81,56,55,40]
-			}
-			
-		]
-		
-	}
-
-	var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
 
 
 	  
